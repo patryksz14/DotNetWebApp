2b9392a10a043e9948c75f3b40af1f7e445aee16
patryksz14 <51371125+patryksz14@users.noreply.github.com>
2021-01-26 16:14:36 +0100

    Added migrations

---
+++ Forum/Migrations/20210126150929_CreateDB.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Forum.Migrations
+{
+    public partial class CreateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(unicode: false, maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    login = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    password = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    nick = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    registration_date = table.Column<DateTime>(type: "datetime", nullable: false),
+                    last_password_changed_date = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    content = table.Column<string>(unicode: false, maxLength: 250, nullable: false),
+                    creator_id = table.Column<int>(nullable: false),
+                    category_id = table.Column<int>(nullable: false),
+                    creation_date = table.Column<DateTime>(type: "datetime", nullable: true),
+                    last_edition_date = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Post_To_Category",
+                        column: x => x.category_id,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Post_To_User",
+                        column: x => x.creator_id,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    content = table.Column<string>(unicode: false, maxLength: 200, nullable: false),
+                    post_id = table.Column<int>(nullable: false),
+                    creator_id = table.Column<int>(nullable: false),
+                    creation_date = table.Column<DateTime>(type: "datetime", nullable: false),
+                    last_edition_date = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comment_To_User",
+                        column: x => x.creator_id,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Comment_To_Post",
+                        column: x => x.post_id,
+                        principalTable: "Post",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PostTag",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    tag_id = table.Column<int>(nullable: false),
+                    post_id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostTag", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PostTags_To_Post",
+                        column: x => x.post_id,
+                        principalTable: "Post",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PostTags_To_Tag",
+                        column: x => x.tag_id,
+                        principalTable: "Tag",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Point",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<int>(nullable: false),
+                    comment_id = table.Column<int>(nullable: false),
+                    date_of_addition = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Point", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Points_To_Comment",
+                        column: x => x.comment_id,
+                        principalTable: "Comment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Points_To_User",
+                        column: x => x.user_id,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_creator_id",
+                table: "Comment",
+                column: "creator_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_post_id",
+                table: "Comment",
+                column: "post_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Point_comment_id",
+                table: "Point",
+                column: "comment_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Point_user_id",
+                table: "Point",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_category_id",
+                table: "Post",
+                column: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_creator_id",
+                table: "Post",
+                column: "creator_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostTag_post_id",
+                table: "PostTag",
+                column: "post_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostTag_tag_id",
+                table: "PostTag",
+                column: "tag_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Point");
+
+            migrationBuilder.DropTable(
+                name: "PostTag");
+
+            migrationBuilder.DropTable(
+                name: "Comment");
+
+            migrationBuilder.DropTable(
+                name: "Tag");
+
+            migrationBuilder.DropTable(
+                name: "Post");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
